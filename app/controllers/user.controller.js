112cf88db5ad3d87826ecf3589eca4b30d9e4dcc
JhamilRuben <64047231+JhamilRuben@users.noreply.github.com>
2022-07-07 16:29:41 -0500

    Initial commit

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,21 @@
+
+
+///antes 4/7
+exports.allAccess = (req, res) => {
+  res.status(200).send("Public Content.");
+};
+
+exports.userBoard = (req, res) => {
+  res.status(200).send("User Contenido.");
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
+
+exports.moderatorBoard = (req, res) => {
+  res.status(200).send("Moderator Content.");
+};
+
+/////--- 
+
---
+++ app/routes/user.routes.js
@@ -0,0 +1,51 @@
+
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/user.controller");
+const controllerFormato = require("../controllers/formato.controller");
+
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.get("/api/test/all", controller.allAccess);
+
+  app.get(
+    "/api/test/user",
+    [authJwt.verifyToken],
+    controller.userBoard
+  );
+
+    // Retrieve all Formatos
+  // app.get(
+  //   "/api/test/user/formatos", 
+  //   [authJwt.verifyToken],
+  //   controllerFormato.userf
+  //   );
+
+  app.get(
+    "/api/test/user/formatos", 
+    [authJwt.verifyToken],
+    controllerFormato.findAll
+  );
+
+  /////////////////////////////////// app.use('/api/user', controller);
+
+  app.get(
+    "/api/test/mod",
+    [authJwt.verifyToken, authJwt.isModerator],
+    controller.moderatorBoard
+  );
+
+  app.get(
+    "/api/test/admin",
+    [authJwt.verifyToken, authJwt.isAdmin],
+    controller.adminBoard
+  );
+
+  
+};
